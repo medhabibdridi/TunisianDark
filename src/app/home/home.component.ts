a30c6601e3115ce7ab75c2cfd11c538dab71c997
medhabibdridi <48842413+medhabibdridi@users.noreply.github.com>
2020-12-11 15:47:50 +0100

    admin navigate button when logged in

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth : AuthService, private router : Router ) { }
 
   ngOnInit(): void {
   }
-
+ go () {
+   this.router.navigateByUrl('/admin')
+ }
 }
